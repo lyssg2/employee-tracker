dcde61a4544e488a9cb122a901085ba5dd40ae7b
Lyss Garcia <alyssa.g200@gmail.com>
2021-12-05 14:39:23 -0800

    added prompts

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+// Import and require mysql2
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection({
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // MySQL password
+        password: 'W1nch3$t3r22',
+        database: 'employees'
+    },
+    console.log(`Connected to the employees database.`)
+);
